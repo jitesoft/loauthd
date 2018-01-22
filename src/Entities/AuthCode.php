430cc92e0bccff4bacf0e49c5157d9e009c68244
Johannes <johannes@jitesoft.com>
2018-01-22 14:13:08 +0100

    Updated AuthCode entity to implement the entity interface. Updated repository test with basic calls.

--- src/Entities/AuthCode.php
+++ src/Entities/AuthCode.php
@@ -1,13 +1,118 @@
 <?php
-
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
   AuthCode.php - Part of the lumen-doctrine-oauth2 project.
 
   © - Jitesoft 2018
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 namespace Jitesoft\OAuth\Lumen\Entities;
 
-class AuthCode {
+use League\OAuth2\Server\Entities\AuthCodeEntityInterface;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
 
+class AuthCode implements AuthCodeEntityInterface {
+
+    /**
+     * @return string
+     */
+    public function getRedirectUri() {
+        // TODO: Implement getRedirectUri() method.
+    }
+
+    /**
+     * @param string $uri
+     */
+    public function setRedirectUri($uri) {
+        // TODO: Implement setRedirectUri() method.
+    }
+
+    /**
+     * Get the token's identifier.
+     *
+     * @return string
+     */
+    public function getIdentifier() {
+        // TODO: Implement getIdentifier() method.
+    }
+
+    /**
+     * Set the token's identifier.
+     *
+     * @param $identifier
+     */
+    public function setIdentifier($identifier) {
+        // TODO: Implement setIdentifier() method.
+    }
+
+    /**
+     * Get the token's expiry date time.
+     *
+     * @return \DateTime
+     */
+    public function getExpiryDateTime() {
+        // TODO: Implement getExpiryDateTime() method.
+    }
+
+    /**
+     * Set the date time when the token expires.
+     *
+     * @param \DateTime $dateTime
+     */
+    public function setExpiryDateTime(\DateTime $dateTime) {
+        // TODO: Implement setExpiryDateTime() method.
+    }
+
+    /**
+     * Set the identifier of the user associated with the token.
+     *
+     * @param string|int $identifier The identifier of the user
+     */
+    public function setUserIdentifier($identifier) {
+        // TODO: Implement setUserIdentifier() method.
+    }
+
+    /**
+     * Get the token user's identifier.
+     *
+     * @return string|int
+     */
+    public function getUserIdentifier() {
+        // TODO: Implement getUserIdentifier() method.
+    }
+
+    /**
+     * Get the client that the token was issued to.
+     *
+     * @return ClientEntityInterface
+     */
+    public function getClient() {
+        // TODO: Implement getClient() method.
+    }
+
+    /**
+     * Set the client that the token was issued to.
+     *
+     * @param ClientEntityInterface $client
+     */
+    public function setClient(ClientEntityInterface $client) {
+        // TODO: Implement setClient() method.
+    }
+
+    /**
+     * Associate a scope with the token.
+     *
+     * @param ScopeEntityInterface $scope
+     */
+    public function addScope(ScopeEntityInterface $scope) {
+        // TODO: Implement addScope() method.
+    }
+
+    /**
+     * Return an array of scopes associated with the token.
+     *
+     * @return ScopeEntityInterface[]
+     */
+    public function getScopes() {
+        // TODO: Implement getScopes() method.
+    }
 }
--- tests/Repositories/Doctrine/AuthCodeRepositoryTest.php
+++ tests/Repositories/Doctrine/AuthCodeRepositoryTest.php
@@ -6,19 +6,54 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 namespace Jitesoft\OAuth\Lumen\Tests\Repositories\Doctrine;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Jitesoft\Log\NullLogger;
+use Jitesoft\OAuth\Lumen\Entities\AuthCode;
 use Jitesoft\OAuth\Lumen\Repositories\Doctrine\AuthCodeRepository;
+use League\OAuth2\Server\Entities\AuthCodeEntityInterface;
+use League\OAuth2\Server\Repositories\AuthCodeRepositoryInterface;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class AuthCodeRepositoryTest extends TestCase {
 
-    public function testGetNewAuthCode() {}
+    /** @var AuthCodeRepositoryInterface */
+    protected $repository;
 
-    public function testPersistNewAuthCode() {}
+    /** @var EntityManagerInterface|Mockery\Mock */
+    protected $entityManagerMock;
 
-    public function testPersistNewAuthCodeError() {}
+    protected function setUp() {
+        parent::setUp();
 
-    public function testRevokeAuthCode() {}
+        $this->entityManagerMock = Mockery::mock(EntityManagerInterface::class);
+        $this->repository        = new AuthCodeRepository($this->entityManagerMock, new NullLogger());
+    }
 
-    public function testIsAuthCodeRevoked() {}
+    public function testGetNewAuthCode() {
+        $this->assertInstanceOf(AuthCodeEntityInterface::class, $this->repository->getNewAuthCode());
+    }
+
+    public function testPersistNewAuthCode() {
+        $this->repository->persistNewAuthCode(new AuthCode());
+    }
+
+    public function testPersistNewAuthCodeError() {
+        $code = new AuthCode();
+
+        $this->repository->persistNewAuthCode($code);
+        $this->repository->persistNewAuthCode($code);
+    }
+
+    public function testRevokeAuthCode() {
+
+        $this->repository->revokeAuthCode('1');
+        $this->repository->revokeAuthCode(2);
+    }
+
+    public function testIsAuthCodeRevoked() {
+        $this->assertTrue($this->repository->isAuthCodeRevoked(1));
+        $this->assertFalse($this->repository->isAuthCodeRevoked(2));
+    }
 
 }
