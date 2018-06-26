ffc2dc93a3206bd231185e0c804e734271a4c602
Johannes <johannes@jitesoft.com>
2018-06-26 08:32:51 +0200

    Licens, Contributing and Readme updates.

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# LOAuthD2
+# LoAuthD
+
+## Lumen oAuth(2) with Doctrine
 
 This package is a bridge for laravel/lumen applications using the doctrine dbal/orm and want's to quickly implement a OAuth2 server for their API.  
 It's built on top of the [League\OAuth2](http://oauth2.thephpleague.com/) package.
+
+## Observe
+
+This project is still under development, it should not be seen as stable nor be used in production.
 
 ## Installation
 
--- src/Http/Controllers/TokenController.php
+++ src/Http/Controllers/TokenController.php
@@ -8,8 +8,10 @@ namespace Jitesoft\Loauthd\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Jitesoft\Exceptions\Lazy\NotImplementedException;
+use Jitesoft\Exceptions\Security\OAuth2\OAuth2Exception;
 use Laravel\Lumen\Routing\Controller;
 use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Exception\OAuthServerException;
 use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
 use Zend\Diactoros\Response;
 
@@ -26,14 +28,27 @@ class TokenController extends Controller {
         $this->factory             = new DiactorosFactory();
     }
 
+    /**
+     * @throws NotImplementedException
+     */
     public function postRefreshTransientToken() {
         throw new NotImplementedException();
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     * @throws OAuth2Exception
+     */
     public function postIssueAccessToken(Request $request) {
         $psrRequest = $this->factory->createRequest($request);
 
-        $out = $this->authorizationServer->respondToAccessTokenRequest($psrRequest, new Response());
+        try {
+            $out = $this->authorizationServer->respondToAccessTokenRequest($psrRequest, new Response());
+        } catch (OAuthServerException $ex) {
+            throw new OAuth2Exception($ex->getMessage(), $ex->getCode(), $ex);
+        }
+
         return new \Illuminate\Http\Response(
             $out->getBody(),
             $out->getStatusCode(),
@@ -41,6 +56,9 @@ class TokenController extends Controller {
         );
     }
 
+    /**
+     * @param Request $request
+     */
     public function deleteAccessToken(Request $request) {
         $request->get('token');
     }
